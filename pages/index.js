075f1a8f1a5e086540ebabbc36947f99e574d1c6
Steven <steven@stevengouws.com>
2020-06-26 13:39:10 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get TESTER by editing <code>pages/index.js</code>
         </p>
 
         <div className="grid">
