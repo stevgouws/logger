5a70dd19bec46121d9979baaf4bbca4ef5de48d8
Steven <steven@stevengouws.com>
2020-07-01 19:00:24 +0100

    adds selected

--- components/NumberGrid.js
+++ components/NumberGrid.js
@@ -1,13 +1,26 @@
 import styles from "./NumberGrid.styl";
+import { useState } from "react";
 
 const numbers = Array.from({ length: 100 }, (el, index) => index + 1);
+
 const NumberGrid = () => {
+  const [selectedNumber, setSelectedNumber] = useState(null);
+
+  function isSelected(number) {
+    return number === selectedNumber;
+  }
   return (
     <div className={styles.numberGrid}>
-      {numbers.map((n) => {
+      {numbers.map((number) => {
         return (
-          <div key={n} className={styles.number}>
-            {n}
+          <div
+            key={number}
+            className={`${styles.number} ${
+              isSelected(number) ? styles.selected : ""
+            }`.trim()}
+            onClick={() => setSelectedNumber(number)}
+          >
+            {number}
           </div>
         );
       })}
