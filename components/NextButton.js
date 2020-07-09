28343b466d7eeafbd844031e4e72e60a0d7d1498
Steven <steven@stevengouws.com>
2020-07-09 09:36:37 +0100

    wip

--- components/NextButton.js
+++ components/NextButton.js
@@ -1,20 +1,37 @@
 import styles from "./NextButton.styl";
 import Link from "next/link";
-import { useState } from "react";
+import { useRef } from "react";
 
-const urls = ["/", "/thoracic", "/", "thoracic"];
+const urls = ["/", "/thoracic", "/typography"];
 
-export const getNextUrl = () => {
-  return "test";
+export const getNextUrl = ({ urls, currentUrl }) => {
+  const currentUrlIndex = urls.indexOf(currentUrl);
+  const nextUrlIndex = currentUrlIndex + 1;
+  const isLastUrl = nextUrlIndex === urls.length;
+  if (isLastUrl) return currentUrl;
+  return urls[nextUrlIndex];
 };
 
 const NextButton = () => {
-  const [currentUrl, setCurrentUrl] = useState("/");
+  console.log("rendering NextButton");
+  let currentUrlRef = useRef("/");
+  console.log("currentUrlRef", currentUrlRef);
+  const nextUrl = getNextUrl({ urls, currentUrl: currentUrlRef.current });
+  console.log("nextUrl", nextUrl);
 
   return (
-    <Link href={getNextUrl()}>
-      <a className={styles.nextButton}>Next</a>
-    </Link>
+    <div
+      onClick={() => {
+        console.log("on click");
+        const random = Math.random();
+        currentUrlRef.current = random;
+        console.log(`after updating ref to ${random}`);
+      }}
+    >
+      <Link href={nextUrl}>
+        <a className={styles.nextButton}>Next {currentUrlRef.current}</a>
+      </Link>
+    </div>
   );
 };
 
