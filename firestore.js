ec9a6f5be15f9d09538a10eb3019787b50178fd8
Steven <steven@stevengouws.com>
2020-07-13 16:29:45 +0100

    fix env

--- firestore.js
+++ firestore.js
@@ -1,18 +1,17 @@
 const admin = require("firebase-admin");
 
 const serviceAccount = {
-  type: process.env.NEXT_PUBLIC_FIREBASE_DB_type,
-  project_id: process.env.NEXT_PUBLIC_FIREBASE_DB_project_id,
-  private_key_id: process.env.NEXT_PUBLIC_FIREBASE_DB_private_key_id,
-  private_key: process.env.NEXT_PUBLIC_FIREBASE_DB_private_key,
-  client_email: process.env.NEXT_PUBLIC_FIREBASE_DB_client_email,
-  client_id: process.env.NEXT_PUBLIC_FIREBASE_DB_client_id,
-  auth_uri: process.env.NEXT_PUBLIC_FIREBASE_DB_auth_uri,
-  token_uri: process.env.NEXT_PUBLIC_FIREBASE_DB_token_uri,
+  type: process.env.FIREBASE_DB_TYPE,
+  project_id: process.env.FIREBASE_DB_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_DB_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_DB_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_DB_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_DB_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_DB_AUTH_URI,
+  token_uri: process.env.FIREBASE_DB_TOKEN_URI,
   auth_provider_x509_cert_url:
-    process.env.NEXT_PUBLIC_FIREBASE_DB_auth_provider_x509_cert_url,
-  client_x509_cert_url:
-    process.env.NEXT_PUBLIC_FIREBASE_DB_client_x509_cert_url,
+    process.env.FIREBASE_DB_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_DB_CLIENT_X509_CERT_URL,
 };
 
 try {
