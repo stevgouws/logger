326e5a0b3a4adf957c7d4b3ea2935534718b5d42
Steven <steven@stevengouws.com>
2020-07-15 15:39:17 +0100

    try to fix private key AGAING

--- firestore.js
+++ firestore.js
@@ -4,7 +4,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_DB_TYPE,
   project_id: process.env.FIREBASE_DB_PROJECT_ID,
   private_key_id: process.env.FIREBASE_DB_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_DB_PRIVATE_KEY,
+  private_key: JSON.parse(process.env.FIREBASE_DB_PRIVATE_KEY),
   client_email: process.env.FIREBASE_DB_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_DB_CLIENT_ID,
   auth_uri: process.env.FIREBASE_DB_AUTH_URI,
