8844478813cc03153f604ff2bdb4b02c6ab55930
Steven <steven@stevengouws.com>
2020-07-29 18:12:32 +0100

    Save page styling & messages

--- components/NextButton.js
+++ components/NextButton.js
@@ -2,14 +2,14 @@ import PropTypes from "prop-types";
 import styles from "./NextButton.styl";
 import Link from "next/link";
 import { useRouter } from "next/router";
-const NextButton = ({ nextUrl = "", onClick }) => {
+const NextButton = ({ nextUrl = "", onClick, primaryText }) => {
   const router = useRouter();
 
   return (
     <div className={styles.nextButton}>
       <div onClick={() => router.back()}>Back</div>
       <Link href={nextUrl}>
-        <a onClick={onClick}>Next</a>
+        <a onClick={onClick}>{primaryText}</a>
       </Link>
     </div>
   );
@@ -18,6 +18,11 @@ const NextButton = ({ nextUrl = "", onClick }) => {
 NextButton.propTypes = {
   nextUrl: PropTypes.string,
   onClick: PropTypes.func.isRequired,
+  primaryText: PropTypes.string,
+};
+
+NextButton.defaultProps = {
+  primaryText: "Next",
 };
 
 export default NextButton;
--- pages/save.js
+++ pages/save.js
@@ -1,8 +1,37 @@
+import PropTypes from "prop-types";
 import { useContext, useState } from "react";
 import Layout from "../components/Layout";
 import { store } from "../store";
 import { useRouter } from "next/router";
 import { todayString } from "../utils";
+import NextButton from "../components/NextButton";
+import styles from "./save.styl";
+
+const DataGrid = ({ state }) => (
+  <div>
+    {Object.entries(state).map(([key, value]) => {
+      return (
+        <div className={styles.data} key={key}>
+          <div className={styles.key}>{key}</div>
+          <div className={styles.value}>{value}</div>
+        </div>
+      );
+    })}
+  </div>
+);
+
+DataGrid.propTypes = {
+  state: PropTypes.object.isRequired,
+};
+
+const Message = ({ text }) => {
+  if (!text) return null;
+  return <div className={styles.message}>{text}</div>;
+};
+
+Message.propTypes = {
+  text: PropTypes.string,
+};
 
 const Save = () => {
   const { dispatch, state } = useContext(store);
@@ -17,7 +46,7 @@ const Save = () => {
 
   async function save() {
     reset();
-    setBusy("Loading...");
+    setBusy("Saving...");
     if (await alreadyHasDataForDate()) {
       setError("You've already logged data for today");
       return;
@@ -48,21 +77,12 @@ const Save = () => {
 
   return (
     <Layout>
-      <div>
-        <h1>Summary</h1>
-        <div>
-          {Object.entries(state).map(([key, value]) => {
-            return (
-              <div key={key}>
-                {key}: {value}
-              </div>
-            );
-          })}
-        </div>
+      <h1>Summary</h1>
+      <div className={styles.mid}>
+        <Message text={error || busy} />
+        {!busy && !error && <DataGrid state={state}></DataGrid>}
       </div>
-      <button onClick={save}>Save</button>
-      {error || busy}
-      {/* <NextButton nextUrl={nextUrl} onClick={updateState} /> */}
+      <NextButton onClick={save} primaryText="Save" />
     </Layout>
   );
 };
