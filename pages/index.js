ddd08b7f5faf9d8fda3328859a4bbc8eee375c2f
Steven <steven@stevengouws.com>
2020-08-08 18:51:54 +0100

    adds MedItems

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Layout from "../components/Layout";
 import NextButton from "../components/NextButton";
 import { useState } from "react";
-import MainHeading from "../components/MainHeading";
 
 export default function Home() {
   const [nextUrl, setNextUrl] = useState("neck");
 
   return (
     <Layout>
-      <MainHeading name="meds" />
       <div>
         <button onClick={() => setNextUrl("meds")}>Log Meds</button>
         <button onClick={() => setNextUrl("neck")}>Log Stats</button>
--- pages/meds.js
+++ pages/meds.js
@@ -1,5 +1,71 @@
-import Page from "../components/Page";
+import PropTypes from "prop-types";
+import Layout from "../components/Layout";
+import MainHeading from "../components/MainHeading";
+import NextButton from "../components/NextButton";
+import styles from "./meds.styl";
 
 export default function Meds() {
-  return <Page name="meds" nextUrl="done" />;
+  return (
+    <Layout>
+      <MainHeading name="meds" />
+      <MedItems />
+      <NextButton />
+    </Layout>
+  );
 }
+
+const MedItems = () => {
+  const medItems = [
+    {
+      name: "Paracetamol",
+      amount: 1000,
+      unit: "mg",
+    },
+    {
+      name: "Ibuprofen",
+      amount: 500,
+      unit: "mg",
+    },
+  ];
+
+  return (
+    <div>
+      {medItems.map((item) => (
+        <MedItem {...item} key={item.name} />
+      ))}
+    </div>
+  );
+};
+
+const MedItem = ({ name, amount, unit }) => {
+  return (
+    <div className={styles.medItem}>
+      <div>
+        {name} {amount} {unit}
+      </div>
+      <Counter />
+    </div>
+  );
+};
+
+MedItem.propTypes = {
+  amount: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  unit: PropTypes.string.isRequired,
+};
+
+const Counter = () => {
+  return (
+    <div className={styles.counter}>
+      <Deviation symbol="-" /> 0 <Deviation symbol="+" />
+    </div>
+  );
+};
+
+const Deviation = ({ symbol }) => {
+  return <span className={styles.deviation}>{symbol}</span>;
+};
+
+Deviation.propTypes = {
+  symbol: PropTypes.string.isRequired,
+};
